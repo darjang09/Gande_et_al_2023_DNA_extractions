5e044c0ff8469c1d3b2bbfcf27862b42ecd894ed
Darjan Gande <68852343+darjang09@users.noreply.github.com>
2023-10-18 16:05:46 +0200

    02_final_extractions_means_and_SD_calculation.R

--- Scripts/02_final_extractions_means_and_SD_calculation.R
+++ Scripts/02_final_extractions_means_and_SD_calculation.R
@@ -8,36 +8,38 @@ library(rcompanion)
 library(FSA)
 library(dplyr)
 
-
 ## Table S2
 
 # Import the data
-df <- read.table("../../All_the_data/06_Results/01_extraction_optimization/Extractions_2022/Statistics/data_for_stats_minerals_FINAL.txt", sep="\t", header=T, stringsAsFactors = T, check.names = FALSE)
+df <- read.table("data_for_stats_minerals_FINAL.txt", sep="\t", header=T, stringsAsFactors = T, check.names = FALSE)
 
-# calculate means
+# Calculate means
 
 means <- tapply(df$Value, list(df$Mineral, df$Buffer, df$Extraction), mean)
 
+# Round the numbers to two decimals
+
 means_table <- as.data.frame(round(means, 2))
 
-# calculate SD
+# Calculate SD
 
 stdev <- tapply(df$Value, list(df$Mineral, df$Buffer, df$Extraction), sd)
 
 sd_table <- as.data.frame(round(stdev, 2))
 
-
 ### Table S1 - unbound DNA
 
-df <- read.table("../../All_the_data/06_Results/01_extraction_optimization/Extractions_2022/Statistics/data_for_stats_unbound_minerals_FINAL.txt", sep="\t", header=T, stringsAsFactors = T, check.names = FALSE)
+df <- read.table("data_for_stats_unbound_minerals_FINAL.txt", sep="\t", header=T, stringsAsFactors = T, check.names = FALSE)
 
-# calculate means
+# Calculate means
 
 means <- tapply(df$Value, list(df$Mineral, df$Buffer, df$Extraction), mean)
 
+# Round the numbers to two decimals
+
 means_table <- as.data.frame(round(means, 2))
 
-# calculate SD
+# Calculate SD
 
 stdev <- tapply(df$Value, list(df$Mineral, df$Buffer, df$Extraction), sd)
 
